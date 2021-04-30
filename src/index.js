0d5e8d55f92d469a369c56f0962136185ac058af
Jim Clark <jim.clark@generalassemb.ly>
2021-04-30 13:19:27 -0700

    Who knows

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router><App /></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
